248634a479d8737bd5e6e139ee15fd870554475b
Adrian Morataya <stevencalavera2@gmail.com>
2025-08-20 12:50:18 -0600

    add: Hello world example in an API

---
+++ MiApi/Controllers/HolaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HolaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Hola Mundo";
+    }
+}
